a78c9d5ec0238eb3f1b2e60536a700dba5adce50
KarenChoquecallata <chkaren29@gmail.com>
2023-05-13 19:45:59 -0400

    prueba2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <div className="App">
        <h1>Karen Choquecallta</h1>
+       <p>parrafo lorem lorem  </p>
+
+       <h1>Karen Choquecallta</h1>
+       <p>parrafo lorem lorem  </p>
+       <h1>Karen Choquecallta</h1>
+       <p>parrafo lorem lorem  </p>
+       <h1>Karen Choquecallta</h1>
+       <p>parrafo lorem lorem  </p>
+       <h1>Karen Choquecallta</h1>
+       <p>parrafo lorem lorem  </p>
+       <h1>Karen Choquecallta</h1>
+       <p>parrafo lorem lorem  </p>
+       
     </div>
   );
 }
